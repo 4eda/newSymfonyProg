543c0ac959f6cb550191fb03509f78e65796928d
4eda <4eda777@mail.ru>
2021-12-14 00:39:05 +0300

    14.12

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-
-
-$('#id_btn_diff').click(
-    function (e){
-        e.preventDefault();
-        $('#id_diff_sear').removeClass('diff-search');
-        $('id_lite_sear').addClass('diff-search');
-    }
-);
